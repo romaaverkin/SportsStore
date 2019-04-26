74813df8b91fcad8b42658f8b7a94e7878a24ff9
Роман Аверкин <romaaverkin@gmail.com>
2019-04-26 15:02:13 +0700

    Сделал первичные настройки и создал класс Product.

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -15,19 +15,34 @@ namespace SportsStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //           Этот расширяющий метод отображает детали исключения,
+            // которое произошло в приложении , что полезно во
+            // время процесса разработки . Он не должен быть включен
+            // в развернутых приложениях; в главе 12 будет показано,
+            // как отключить данное средство
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
+            //            Этот расширяющий метод добавляет простое сообще ние
+            // в НТТР - ответы, которые иначе бы не имели тела,
+            // такие как ответы 404 - Not Found(404 - не найдено)
+            app.UseStatusCodePages();
+
+            //            Этот расширяющий метод включает поддержку для
+            // обслуживания статического содержимого из папки
+            // wwwroot
+            app.UseStaticFiles();
+
+            //            Этот расширяющий метод включает инфраструктуру
+            // ASP.NET Core MVC со стандартной конфигурацией
+            // (которая позже в процессе разработки будет изменена)
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
